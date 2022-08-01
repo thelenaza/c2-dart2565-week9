0fa4ea45a7fdcbb892f07c3061644d7573b5f4bc
thelenaza <kaxtie07@gmail.com>
2022-08-01 09:13:50 +0700

    first commit

---
+++ bin/dart_week9.dart
@@ -0,0 +1,15 @@
+
+void main(){
+  //Switch Case Statement
+  int day = 2;
+ switch(day){
+   //Statement
+   case 1:
+   print("Sunday");
+   break;
+   default:
+   print("ERROR");
+   break;
+ }
+
+}
